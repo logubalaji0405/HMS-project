aea515ba87289c179305b07a898f0f5ee4639750
012balaji <144882695+012balaji@users.noreply.github.com>
2026-04-15 14:54:01 +0530

    deploy ready

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -49,7 +49,7 @@ WSGI_APPLICATION = 'hospital_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
 
